a505427e900e6c85c491fe361b3b76cec0c4b2d6
Jérôme LELEU <leleuj@gmail.com>
2019-08-20 08:11:51 +0200

    update dependencies

--- src/main/java/org/pac4j/demo/shiro/CustomAuthorizer.java
+++ src/main/java/org/pac4j/demo/shiro/CustomAuthorizer.java
@@ -1,6 +1,6 @@
 package org.pac4j.demo.shiro;
 
-import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang.StringUtils;
 import org.pac4j.core.authorization.authorizer.ProfileAuthorizer;
 import org.pac4j.core.context.WebContext;
 import org.pac4j.core.exception.HttpAction;
